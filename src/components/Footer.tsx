5a7bff3b33ff799e0bc96425a05afc90bc2978a0
Mahmudul Alam <expelmahmud@gmail.com>
2023-07-04 20:01:44 +0600

    add(UI): footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { Component } from "solid-js";
+
+export const Footer: Component = () => {
+  return (
+    <footer class="py-10">
+      <p class="text-center text-white text-sm">Made with ☕️, 💻, and ❤️</p>
+    </footer>
+  );
+};
