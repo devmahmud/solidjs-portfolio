ed7dae9924950304bff380e9bad1e83f03f059b2
Mahmudul Alam <expelmahmud@gmail.com>
2023-07-04 20:03:20 +0600

    add: logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'solid-js';
+import { A } from 'solid-start';
+
+export const Logo: Component = () => {
+  return (
+    <A
+      href="/"
+      class="font-serif rounded-lg text-white text-3xl font-bold hover:underline"
+    >
+      M.
+    </A>
+  );
+};
