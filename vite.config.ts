d3bc4a89957eb3672acffac92069afb34c61ff63
Mahmudul Alam <expelmahmud@gmail.com>
2023-07-04 20:02:11 +0600

    fix: configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,19 @@ import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 
+import { settingsPlugin } from './plugins/yaml-settings';
+import { githubPlugin } from './plugins/github';
+import { postsPlugin } from './plugins/posts';
+import { sitemapGenerator } from './plugins/sitemap';
+
 export default defineConfig({
-  plugins: [solid(), eslint()],
+  base: '/', // base URL for the projects
+  plugins: [
+    solid({ ssr: false, extensions: ['.mdx', '.md'] }),
+    eslint(),
+    settingsPlugin({ relativePath: './src/settings.yml' }),
+    githubPlugin({ username: 'devmahmud' }),
+    postsPlugin(),
+    sitemapGenerator(),
+  ],
 });
