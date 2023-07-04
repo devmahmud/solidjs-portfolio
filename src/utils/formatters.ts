4234d6e0b774c92e1f3992e7b885f12251c30430
Mahmudul Alam <expelmahmud@gmail.com>
2023-07-04 20:02:48 +0600

    add: text formatter

---
+++ src/utils/formatters.ts
@@ -0,0 +1,16 @@
+import { config } from 'virtual:config';
+
+export function parseBioText(text: string | undefined): string {
+  if (text == null) return '';
+
+  const tagRegex = /@(\w*)/g;
+  const linksMap: Map<string, string> = new Map(Object.entries(config.links));
+
+  return text?.replace(tagRegex, (originalText, name: string) => {
+    const link = linksMap.get(name);
+
+    if (link)
+      return `<a class="text-pink-400" target="blank" href="${link}">${originalText}</a>`;
+    return originalText;
+  });
+}
