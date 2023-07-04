47712068eebb27215d4b26745af5d35581950a40
Mahmudul Alam <expelmahmud@gmail.com>
2023-07-04 20:03:01 +0600

    add: resume page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,31 +1,23 @@
-import { A } from "solid-start";
+import type { Component } from 'solid-js';
+import { Title } from 'solid-start';
+import { config } from 'virtual:config';
+import NotFound from '~/components/NotFound';
+import { ButtonLink } from '~/components/shared';
 
-export default function NotFound() {
+const { site } = config;
+
+export const Page404: Component = () => {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Not Found
-      </h1>
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>
-      </p>
-    </main>
+    <>
+      <Title>{site.title} - Not found</Title>
+
+      <NotFound>
+        <ButtonLink to="/" class="border-pink-500 text-pink-500">
+          Go Home
+        </ButtonLink>
+      </NotFound>
+    </>
   );
-}
+};
+
+export default Page404;
---
+++ src/routes/resume.tsx
@@ -0,0 +1,184 @@
+import type { Component } from 'solid-js';
+import { For } from 'solid-js';
+import SolidMarkdown from 'solid-markdown';
+import { profile } from 'virtual:github';
+import { config } from 'virtual:config';
+import { parseBioText } from '~/utils/formatters';
+import { ButtonLink, ChildrenProps } from '~/components/shared';
+import { LinkedinIcon } from '~/components/icons';
+
+const { links, cover_letter, relevant_experiences, education, skills } = config;
+
+export const Resume: Component = () => {
+  return (
+    <div class="grid gap-4 sm:grid-cols-[1fr_2fr] text-gray-200">
+      <div class="print:grid print:grid-cols-2">
+        <Sidebar />
+        <Skills />
+      </div>
+      <div>
+        <CoverLetter />
+        <Experiences />
+        <Education />
+      </div>
+    </div>
+  );
+};
+
+const Sidebar: Component = () => {
+  return (
+    <div class="shadow mb-4 grid gap-1 bg-gray-900 h-[inherit] text-center border-bottom border-b-10 border-pink-500">
+      <div class="text-gray-900 relative h-[16rem]">
+        <div class="bg-blue-400 h-[60%] p-2" />
+        <img
+          class="h-[180px] w-[180px] rounded-full mt-[-100px] border-gray-900 mx-auto"
+          alt="Profile logo"
+          src={profile.avatar_url}
+        />
+      </div>
+
+      <div class="info grid gap-4 p-6 pt-0">
+        <h2 class="text-2xl font-semibold">{profile.name}</h2>
+        <p innerHTML={parseBioText(profile.bio)} />
+
+        <ButtonLink
+          class="text-pink-400 w-full mx-auto print:hidden"
+          to={config.resume_url}
+          target="blank"
+        >
+          Resume
+        </ButtonLink>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
+
+const CoverLetter: Component = () => {
+  return (
+    <CardLayout title="Cover Letter" class="print:break-after-page">
+      <For each={cover_letter.split('\n')}>
+        {(text) => (
+          <p class="my1 mx0 text-sm">
+            <SolidMarkdown
+              components={{
+                p: (props) => <p class="my4" {...props} />,
+                a: (props) => (
+                  <a class="text-pink-300 hover:text-pink-400" {...props} />
+                ),
+              }}
+            >
+              {text}
+            </SolidMarkdown>
+          </p>
+        )}
+      </For>
+    </CardLayout>
+  );
+};
+
+const Experiences: Component = () => {
+  return (
+    <CardLayout title="Relevant Experiences">
+      <div class="grid gap-6 my4">
+        <For each={relevant_experiences}>
+          {(exp) => (
+            <div class="text-md grid gap-1">
+              <h4 class="text-base fw500">{exp.job_title}</h4>
+              <p class="text-gray-300 text-sm fw100">
+                {exp.company}, {exp.dates_employed}
+              </p>
+              <p class="mt2 text-sm">{exp.description}</p>
+              <div>
+                <For each={exp.tech_stack}>
+                  {(tech) => (
+                    <span class="mr1 text-sm text-blue-300 hover:text-blue-200 hover:cursor-pointer">{`#${tech} `}</span>
+                  )}
+                </For>
+              </div>
+            </div>
+          )}
+        </For>
+
+        <div class="text-center">
+          <ButtonLink
+            target="blank"
+            to={links.linkedin}
+            class="mx-auto gap-2 text-xs inline-flex items-end py-1 text-blue-400 hover:underline"
+          >
+            <span>See more experiences</span>
+            <span>
+              <LinkedinIcon />
+            </span>
+          </ButtonLink>
+        </div>
+      </div>
+    </CardLayout>
+  );
+};
+
+const Education: Component = () => {
+  return (
+    <CardLayout title="Education">
+      <For each={education}>
+        {(exp) => (
+          <div class="my-2 text-sm grid gap-1 mb-6">
+            <h4 class="text-base fw500">{exp.institution}</h4>
+
+            <p class="fw200">
+              <span class="text-gray-500">{exp.period} - </span>
+              {exp.field_of_study}
+            </p>
+          </div>
+        )}
+      </For>
+    </CardLayout>
+  );
+};
+
+const Skills: Component = () => {
+  return (
+    <CardLayout title="Skills">
+      <For each={Object.entries(skills)}>
+        {([key, skills]: [string, any[]]) => (
+          <div class="my-2 text-sm grid gap-1">
+            <h4 class="text-base font-medium">{key}</h4>
+            <div class="grid m-2 grid-cols-6 gap-2">
+              <For each={skills}>
+                {(skill) => (
+                  <img
+                    data-tooltip-target={skill.name + skill.rate}
+                    src={skill.icon}
+                    alt={skill.name}
+                    class="w-[20px] h-[20px]"
+                    title={`${skill.name} - ${Array(skill.rate)
+                      .fill('☆')
+                      .join('')}`}
+                  />
+                )}
+              </For>
+            </div>
+          </div>
+        )}
+      </For>
+    </CardLayout>
+  );
+};
+
+interface CardLayoutProps extends ChildrenProps {
+  title: string;
+  class?: string;
+}
+
+const CardLayout: Component<CardLayoutProps> = (props) => {
+  return (
+    <div
+      class={`mb-4 p-4 bg-gray-800 border-bottom border-b-10 border-blue-300 ${props.class}`}
+    >
+      <h3 class="text-lg font-semibold py-2">{props.title}</h3>
+
+      <div>{props.children}</div>
+    </div>
+  );
+};
